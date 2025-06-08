65c3455b759abb226e1df10f4c5f17909c342c39
keroojoseph <keroojoseph0@gmail.com>
2025-06-09 01:45:57 +0300

    modified on file arithmetic operation add elif

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -7,7 +7,7 @@ def perform_operation(num1, num2, operation):
         case 'divide':
             if num2 == 0:
                 return 'Not allow divide on zero'
-            else:
+            elif num2 > 0:
                 return num1 / num2
         case 'multiply':
             return num1 * num2
