2cdbbaa4508bf163d6b0d7e5d23a1338d2f5ba85
keroojoseph <keroojoseph0@gmail.com>
2025-06-09 01:40:44 +0300

    add arithmetic_operation and main files

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,13 @@
+def perform_operation(num1, num2, operation):
+    match operation:
+        case 'add':
+            return num1 + num2
+        case 'subtract':
+            return num1 - num2
+        case 'divide':
+            if num2 == 0:
+                return 'Not allow divide on zero'
+            else:
+                return num1 / num2
+        case 'multiply':
+            return num1 * num2
