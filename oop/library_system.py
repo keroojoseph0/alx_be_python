7ec3ab4d79cde244af5b541e9e22d9efc1147de7
keroojoseph <keroojoseph0@gmail.com>
2025-06-24 11:51:17 +0300

    Add library system file and main testj

---
+++ oop/library_system.py
@@ -0,0 +1,32 @@
+class Book:
+    def __init__(self, title: str, author: str):
+        self.title = title
+        self.author = author
+
+class EBook(Book):
+    def __init__(self, title: str, author: str, file_size: int):
+        super().__init__(title, author)
+        self.file_size = file_size
+
+class PrintBook(Book):
+    def __init__(self, title: str, author: str, page_count: int):
+        super().__init__(title, author)
+        self.page_count = page_count
+
+class  Library:
+    books = []
+
+    def add_book(self, book):
+        self.book = book
+        self.books.append(self.book)
+
+    def list_books(self):
+        for book in self.books:
+            class_name = book.__class__.__name__
+            if class_name == "Book":
+                print(f"{class_name}: {book.title} by {book.author}")
+            
+            elif class_name == "EBook":
+                print(f"{class_name}: {book.title} by {book.author}, File Size: {book.file_size}KB")
+            else:
+                print(f"{class_name}: {book.title} by {book.author}, Page Count: {book.page_count}")
